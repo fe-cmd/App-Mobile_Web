91f9ff579fc81fa2bafeaa68dd1b833605a10c5b
Joshua <oluferonmijoshua@gmail.com>
2022-05-31 13:40:28 +0100

    First commit

--- geoip.js
+++ geoip.js
@@ -1,20 +1,28 @@
 function geoip_country_name() { return 'Bangladesh'; }
-function geoip_city()         { return 'Dhaka'; }
-function geoip_region()       { return '81'; }
-function geoip_region_name()  { return 'Dhaka'; }
-function geoip_latitude()     { return '23.7231'; }
-function geoip_longitude()    { return '90.4086'; }
-function geoip_postal_code()  { return ''; }
-function geoip_area_code()    { return ''; }
-function geoip_metro_code()   { return ''; }
 
-$(document).ready(function(){
- 
-    setTimeout(function(){
+function geoip_city() { return 'Dhaka'; }
+
+function geoip_region() { return '81'; }
+
+function geoip_region_name() { return 'Dhaka'; }
+
+function geoip_latitude() { return '23.7231'; }
+
+function geoip_longitude() { return '90.4086'; }
+
+function geoip_postal_code() { return ''; }
+
+function geoip_area_code() { return ''; }
+
+function geoip_metro_code() { return ''; }
+
+$(document).ready(function() {
+
+    setTimeout(function() {
         $('body').addClass('loaded');
-        $('h1').css('color','#FFFFFF');
-   }, 26000);
- 
+        $('h1').css('color', '#FFFFFF');
+    }, 26000);
+
 });
 
 
@@ -31,20 +39,20 @@ window.onscroll = function() {
 };
 
 function scrollFunction() {
-  if (document.body.scrollTop > 100 || document.documentElement.scrollTop > 100) {
-    mybutton.style.display = "block";
-    myclick.style.display = "block";
-    myhand.style.display = "block";
-  } else {
-    mybutton.style.display = "none";
-    myclick.style.display = "none";
-    myhand.style.display = "none";
-  }
+    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 100) {
+        mybutton.style.display = "block";
+        myclick.style.display = "block";
+        myhand.style.display = "block";
+    } else {
+        mybutton.style.display = "none";
+        myclick.style.display = "none";
+        myhand.style.display = "none";
+    }
 }
 
 
 // When the user clicks on the button, scroll to the top of the document
 function topFunction() {
-  document.body.scrollTop = 0; // For Safari
-  document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+    document.body.scrollTop = 0; // For Safari
+    document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
 }
